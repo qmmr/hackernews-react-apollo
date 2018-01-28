cf222888a32a8b12520154d81ee70ef4ef3a2e16
Marcin Kumorek <kumeek@gmail.com>
2018-01-28 20:14:11 +0100

    Add <Link /> and <LinkList />

---
+++ src/components/Link.js
@@ -0,0 +1,20 @@
+/* eslint-disable react/prop-types */
+import React, { Component } from 'react'
+
+class Link extends Component {
+  render() {
+    return (
+      <div>
+        <div>
+          {this.props.link.description} ({this.props.link.url})
+        </div>
+      </div>
+    )
+  }
+
+  _voteForLink = async () => {
+    // ... you'll implement this in chapter 6
+  }
+}
+
+export default Link
