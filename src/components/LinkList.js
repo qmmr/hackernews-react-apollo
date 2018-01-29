733e2cb61d5848a1f30974d8ddb61265f457f44d
Marcin Kumorek <marcin@kalohq.com>
2018-01-29 22:26:09 +0000

    Implement voting with update of the UI

--- src/components/LinkList.js
+++ src/components/LinkList.js
@@ -6,6 +6,18 @@ import gql from 'graphql-tag'
 import { graphql } from 'react-apollo'
 
 class LinkList extends Component {
+  _updateCacheAfterVote = (store, createVote, linkId) => {
+    // 1
+    const data = store.readQuery({ query: FEED_QUERY })
+
+    // 2
+    const votedLink = data.feed.links.find(link => link.id === linkId)
+    votedLink.votes = createVote.link.votes
+
+    // 3
+    store.writeQuery({ query: FEED_QUERY, data })
+  }
+
   render() {
     if (this.props.feedQuery && this.props.feedQuery.loading) {
       return <div>Loading</div>
@@ -17,7 +29,13 @@ class LinkList extends Component {
 
     const linksToRender = this.props.feedQuery.feed.links
 
-    return <div>{linksToRender.map((link, index) => <Link key={link.id} index={index} link={link} />)}</div>
+    return (
+      <div>
+        {linksToRender.map((link, index) => (
+          <Link key={link.id} index={index} link={link} updateStoreAfterVote={this._updateCacheAfterVote} />
+        ))}
+      </div>
+    )
   }
 }
 
