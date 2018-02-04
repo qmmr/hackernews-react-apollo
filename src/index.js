fcfa42998e2b93f4ea665747c57293da059e8447
Marcin Kumorek <kumeek@gmail.com>
2018-02-04 16:22:10 +0100

    Add ws link to client

--- src/index.js
+++ src/index.js
@@ -1,16 +1,18 @@
 import './styles/index.css'
 
-import { BrowserRouter } from 'react-router-dom'
+import { ApolloLink, split } from 'apollo-client-preset'
+
+import { AUTH_TOKEN } from './constants'
 import { ApolloClient } from 'apollo-client'
-import { ApolloLink } from 'apollo-client-preset'
 import { ApolloProvider } from 'react-apollo'
 import App from './components/App'
+import { BrowserRouter } from 'react-router-dom'
 import { HttpLink } from 'apollo-link-http'
 import { InMemoryCache } from 'apollo-cache-inmemory'
 import React from 'react'
 import ReactDOM from 'react-dom'
-
-import { AUTH_TOKEN } from './constants'
+import { WebSocketLink } from 'apollo-link-ws'
+import { getMainDefinition } from 'apollo-utilities'
 import registerServiceWorker from './registerServiceWorker'
 
 const middlewareAuthLink = new ApolloLink((operation, forward) => {
@@ -26,8 +28,28 @@ const middlewareAuthLink = new ApolloLink((operation, forward) => {
 
 const httpLink = new HttpLink({ uri: 'http://localhost:4000' })
 
+const wsLink = new WebSocketLink({
+  uri: 'ws://localhost:4000',
+  options: {
+    reconnect: true,
+    connectionParams: {
+      authToken: localStorage.getItem(AUTH_TOKEN)
+    }
+  }
+})
+
+const httpLinkWithAuthToken = middlewareAuthLink.concat(httpLink)
+const link = split(
+  ({ query }) => {
+    const { kind, operation } = getMainDefinition(query)
+    return kind === 'OperationDefinition' && operation === 'subscription'
+  },
+  wsLink,
+  httpLinkWithAuthToken
+)
+
 const client = new ApolloClient({
-  link: middlewareAuthLink.concat(httpLink),
+  link,
   cache: new InMemoryCache()
 })
 
@@ -37,7 +59,7 @@ ReactDOM.render(
       <App />
     </ApolloProvider>
   </BrowserRouter>,
-  document.getElementById('root'),
+  document.getElementById('root')
 )
 
 registerServiceWorker()
